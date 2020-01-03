9b3fda22cd9e20246c267970d25b1b19c54be29f
Iskander <iskangizatullin@gmail.com>
2020-01-03 23:18:16 +0300

    completed task

---
+++ javascript-1/script.js
@@ -0,0 +1,15 @@
+function sum(a) 
+{
+  if(a == undefined)
+	return 0;
+
+  function innerFunc(b) {
+	if(b == undefined)
+		return a;
+	else
+		a += b;
+    return innerFunc;
+  }
+
+  return innerFunc;
+}
---
+++ javascript-1/test.js
@@ -0,0 +1,17 @@
+var assert = require('chai').assert;
+
+var fs = require('fs');
+var vm = require('vm');
+var path = 'script.js';
+
+var code = fs.readFileSync(path);
+vm.runInThisContext(code);
+
+describe("sum", function() {
+  it("Если выполнена без аргументов, то возвращает значение суммы всех переданных до этого значений", function() {
+    assert.equal(sum(1)(2)(3)(4)(5)(), 1+2+3+4+5);
+  });
+   it("Если выполнена без аргументов сразу же, возвращает 0", function() {
+    assert.equal(sum(), 0);
+  });
+});
