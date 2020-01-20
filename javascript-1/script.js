a571749b1951ebeadab00e7bab85586737bb050c
Iskander <iskangizatullin@gmail.com>
2020-01-20 21:07:03 +0300

    format fix

--- javascript-1/script.js
+++ javascript-1/script.js
@@ -1,15 +1,14 @@
-function sum(a) 
-{
-  if(a == undefined)
-	return 0;
+function sum(a) {
+    if (a == undefined)
+        return 0;
 
-  function innerFunc(b) {
-	if(b == undefined)
-		return a;
-	else
-		a += b;
+    function innerFunc(b) {
+        if (b == undefined)
+            return a;
+        else
+            a += b;
+        return innerFunc;
+    }
+
     return innerFunc;
-  }
-
-  return innerFunc;
 }
