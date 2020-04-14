6df7398d045ae9c5522de80e89beade2cf9e13e1
Iskander <iskangizatullin@gmail.com>
2020-04-14 19:36:10 +0300

    frame

---
+++ react-2/src/Components/WeatherApp.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {CityInput} from "./CityInput";
+
+export class WeatherApp extends React.Component {
+    constructor() {
+        super();
+        this.state = {cityInput : ""};
+        this.handleCityInputChange = this.handleCityInputChange.bind(this);
+    }
+
+    handleCityInputChange(inputChange) {
+        this.setState({
+            cityInput: inputChange
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Weather App</h1>
+                <CityInput input={this.state.cityInput} onTextChange={this.handleCityInputChange}/>
+                <h3>Chosen city {this.state.cityInput}</h3>
+            </div>
+        );
+    }
+}
