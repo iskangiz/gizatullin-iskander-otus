56efaf6908ee1cb61ae0177f62e77a5b9480abc2
Iskander <iskangizatullin@gmail.com>
2020-05-16 18:40:01 +0300

    updated app component

--- angular-1/language-app/src/app/app.module.ts
+++ angular-1/language-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecentlyAddedComponent } from './recently-added/recently-added.component';
+import { GoComponent } from './go/go.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecentlyAddedComponent,
+    GoComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
