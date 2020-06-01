f69e811487d0c6698481e194f68c7051bbe52d0f
Iskander <iskangizatullin@gmail.com>
2020-06-01 23:20:04 +0300

    fix to promise

--- node-2/tree.js
+++ node-2/tree.js
@@ -1,24 +1,30 @@
 const fs = require('fs');
 const { resolve } = require('path');
+const { readdir } = require('fs').promises;
 
 let path = process.argv.slice(2)[0];
 if(path === undefined) {
 	path = './';
 }
 
-function getFilesTree(path) {
-	fs.readdir(path, {withFileTypes: true}, (err, files) => {
-		if (files == null)
-			return;
-		for (const file of files) {
-			if (file.isDirectory()) {
-				const combinedPath = resolve(path, file.name);
-				getFilesTree(combinedPath)
-			} else {
-				console.log(resolve(path, file.name));
-			}
+async function getFilesTree(path) {
+	let result = {files: [], dirs: []};
+
+	const files = await readdir(path, { withFileTypes: true });
+
+	for (const file of files) {
+		if (file.isDirectory()) {
+			const combinedPath = resolve(path, file.name);
+			result.dirs.push(await getFilesTree(combinedPath));
+		} else {
+			result.files.push(resolve(path, file.name));
 		}
-	});
+	}
+
+	return result;
 }
 
-getFilesTree(path);
+
+(async () => {
+	console.log(JSON.stringify( await getFilesTree(path)));
+})();
