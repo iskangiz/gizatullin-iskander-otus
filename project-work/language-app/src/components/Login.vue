40a58892039be90e2d448526d641660c8a660ece
Iskander <iskangizatullin@gmail.com>
2020-06-10 23:46:01 +0300

    added login form

---
+++ project-work/language-app/src/components/Login.vue
@@ -0,0 +1,100 @@
+<template>
+    <v-dialog max-width="500px" v-model="show" persistent>
+        <v-form ref="form">
+            <v-card>
+                <v-alert v-model="loginFailure" type="error">Error while logging</v-alert>
+                <v-card-title>
+                    <span class="headline">Login</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col>
+                                <v-text-field label="Login*"
+                                              required
+                                              :rules="[v => !!v || 'Login is required']"
+                                              v-model="login"></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <v-text-field label="Password"
+                                              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                              hint="example of helper text only on focus"
+                                              :type="showPassword ? 'text' : 'password'"
+                                              :rules="[v => !!v || 'Password is required']"
+                                              @click:append="showPassword = !showPassword"
+                                              v-model="password">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDialog">Close</v-btn>
+                    <v-btn color="blue darken-1" text @click="loginBtn">Login</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-form>
+    </v-dialog>
+</template>
+
+<script>
+
+    import {mapMutations, mapActions} from "vuex";
+    export default {
+        name: "Login",
+        props: {
+            value: Boolean
+        },
+        methods: {
+            ...mapMutations([
+                "addUserInputNumber",
+                "deleteUserInputNumber",
+                "setUserEqualitySign",
+                "setSettings"]),
+            ...mapActions([
+                'loginRequest',
+                'submitTask'
+            ]),
+            loginBtn() {
+                if (this.$refs.form.validate()) {
+                    this.loginRequest({"login": this.$data.login, "password": this.$data.password}).then(() => {
+                        this.$data.loginFailure = false;
+                        this.show = false;
+                    }).catch((er) => {
+                        console.log(er);
+                        this.$data.loginFailure = true
+                    });
+                }
+            },
+            closeDialog() {
+                this.$refs.form.reset();
+                this.show = false;
+                this.$data.loginFailure= false;
+            }
+        },
+        data: () => ({
+            login: "",
+            password: "",
+            showPassword: false,
+            loginFailure: false
+        }),
+        computed: {
+            show: {
+                get () {
+                    return this.value
+                },
+                set (value) {
+                    this.$emit('input', value)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ project-work/language-app/src/components/Vocabulary.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-row>
+        <v-col col="4">category</v-col>
+        <v-col cols="8">
+            <v-row v-for="word in words" :key="word.id">
+                <v-col>{{word.title}}</v-col>
+                <v-col>{{word.url}}</v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    qwe
+                    <v-btn icon @click="playAudio()">
+                        <v-icon>mdi-volume-high</v-icon>
+                    </v-btn>
+                </v-col>
+                <v-col>фыв</v-col>
+            </v-row>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "Vocabulary",
+        data: () => ({
+            words: [
+                {id: 1, title: "Home", url: "/"},
+                {id: 2, title: "Settings", url: "/Settings"}
+            ]
+        }),
+        methods: {
+            playAudio() {
+                alert('щас спою');
+                new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3').play();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
